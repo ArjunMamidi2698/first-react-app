0d37cfd739e905b9b50256bfe220a764c80b5dc7
Mallikharjuna Mamidi <mallikharjuna.mamidi@constellaintelligence.com>
2022-01-11 19:43:45 +0530

    added action handler

--- src/components/muiPersons/PersonCard.js
+++ src/components/muiPersons/PersonCard.js
@@ -1,3 +1,4 @@
+import { Done } from "@mui/icons-material";
 import Edit from "@mui/icons-material/Edit";
 import {
 	Button,
@@ -5,6 +6,7 @@ import {
 	CardActions,
 	CardContent,
 	CardHeader,
+	TextField,
 } from "@mui/material";
 import { useState } from "react";
 import "./PersonCard.css";
@@ -22,44 +24,60 @@ function PersonCard(props) {
 
 	//// can use from props instead of hardcoded
 	// let personName = props.name;
-	const [ personName, setPersonName ] = useState( props.name );
-	const personNameParts = personName.split(" ");
-	let logo = personName[0];
-	if (personNameParts.length === 1) logo += personName[1];
-	else
-		logo = logo += personNameParts
-			.filter((val, index) => index === personNameParts.length - 1)
-			.map((val) => val.trim().substring(0, 1))
-			.join("");
-	const age = props.age;
-	const email = props.email;
-	// const now = props.date.toLocaleString(); // Cannot send date object directly
-	const [ now, setNow ] = useState( props.date.toLocaleString() ); // Cannot send date object directly
-	const editPerson = () => {
-		setPersonName( "arjun" );
-		setNow( new Date().toLocaleString() );
-		setEditView( true );
+		//// can use useState for dynamic update in the components
+	const [personName, setPersonName] = useState(props.name);
+	const [ age, setAge ] = useState( props.age );
+	const [ email, setEmail ] = useState( props.email );
+	const [now, setNow] = useState(props.date.toLocaleString()); // Cannot send date object directly
+	const [editView, setEditView] = useState(false);
+
+	// helpers
+	const getLogo = () => {
+		const personNameParts = personName.split(" ");
+		let logo = personName[0];
+		if (personNameParts.length === 1) logo += personName[1];
+		else
+			logo = logo += personNameParts
+				.filter((val, index) => index === personNameParts.length - 1)
+				.map((val) => val.trim().substring(0, 1))
+				.join("");
+		return logo;
 	};
 
-	const [ editView, setEditView ] = useState( false );
+	// actions
+	const savePerson = () => {
+		setPersonName("arjun");
+		setNow(new Date().toLocaleString());
+	};
+	const cardActionHandler = () => {
+		if( editView ) {
+			savePerson();
+		}
+		setEditView( !editView );
+	};
 	return (
 		<Card className="person-card">
 			<CardHeader
-				title={logo.toUpperCase()}
+				title={getLogo().toUpperCase()}
 				className="person-card__logo"
 			/>
 			<CardContent>
 				{/* we can use dynamic data using {}*/}
-				<div className="person-card__name">{personName}</div>
+				{
+					editView ? <TextField placeholder="Person Name"></TextField> : <div className="person-card__name">{personName}</div>
+				}
 				<div className="person-card__age">{age}</div>
 				<div className="person-card__email">{email}</div>
+				
 				<div className="person-card__loggedIn">Updated: {now}</div>
 			</CardContent>
 			<CardActions>
-				<Button startIcon={<Edit />} sx={{"margin-left":"auto"}} onClick={editPerson}>
-					{
-						editView ? "Done": "Edit"
-					}
+				<Button
+					startIcon={editView ? <Done /> : <Edit />}
+					sx={{ "margin-left": "auto" }}
+					onClick={cardActionHandler}
+				>
+					{editView ? "Done" : "Edit"}
 				</Button>
 			</CardActions>
 		</Card>
